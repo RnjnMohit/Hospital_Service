556fe5f795c082299d7ea66447549be1c101b527
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2024-06-09 19:41:00 +0530

    navbar linked

--- my-hospital/src/components/Navbar.jsx
+++ my-hospital/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiSearch } from "react-icons/fi";
-import AboutUs from '../pages/AboutUs';
 
 function Navbar() {
   return (
-    <nav className='fixed  top-0 left-0 right-0 flex justify-between items-center p-6 bg-transparent text-blue-950 font-bold z-10'>
+    <nav className='fixed top-0 left-0 right-0 flex justify-between items-center p-6 bg-transparent text-blue-950 font-bold z-10'>
       <div className='flex space-x-6 text-xl'>
-        <h2 className='cursor-pointer'>Home</h2>
-        <h2 className='cursor-pointer'>Doctors</h2>
-        <h2 className='cursor-pointer'>Services</h2>
-        <h2 className='cursor-pointer'>About Us</h2>
+        <Link to="/" className='cursor-pointer'>Home</Link>
+        <Link to="/doctors" className='cursor-pointer'>Doctors</Link>
+        <Link to="/appointments" className='cursor-pointer'>Appointments</Link>
+        <Link to="/aboutus" className='cursor-pointer'>About Us</Link>
       </div>
       <div className='flex space-x-6 items-center'>
         <FiSearch className='text-2xl cursor-pointer' />
