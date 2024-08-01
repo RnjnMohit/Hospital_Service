a969bad150ae2d56e17d818f6acf24a39e8f91de
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2024-08-01 09:46:00 +0530

    searchbar

--- my-hospital/src/components/SearchBar.jsx
+++ my-hospital/src/components/SearchBar.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearchPlus } from "react-icons/fa";
 import { IoMdCloseCircle } from "react-icons/io";
 
 function SearchBar({ isOpen, onClose }) {
   if (!isOpen) return null;
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    console.log('Search query:', query);
+  }
+
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  }
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-20 space-x-4'>
       <div className='bg-custom-blue p-4 shadow-lg w-full max-w-md rounded-full flex items-center space-x-4'>
         <input
           type='text'
+          value={query}
           placeholder='Search your Query...'
+          onChange={handleInputChange}
           className='w-80 p-4 bg-blue-950 border-none rounded-full'
         />
-        <button className='bg-slate-800 rounded-full text-3xl w-16 h-16 flex items-center justify-center hover:scale-105 ease-in-out duration-300'>
+        <button onClick={handleSearch} className='bg-slate-800 rounded-full text-3xl w-16 h-16 flex items-center justify-center hover:scale-105 ease-in-out duration-300'>
           <FaSearchPlus className='text-custom-blue' />
         </button>
       </div>
